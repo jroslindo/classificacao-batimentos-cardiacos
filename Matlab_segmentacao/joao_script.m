1eeef1a62f5237ad15877d54007af227d54302dd
João Paulo Roslindo <jroslindotab@gmail.com>
2020-07-22 13:43:32 -0300

    Fazendo python chamar matlab
    Foi feito uma função para elevar a prioridade do python
    Foi feito uma funcao pra chamar o matlab retornando parametro

--- Matlab_segmentacao/joao_script.m
+++ Matlab_segmentacao/joao_script.m
@@ -1,10 +1,13 @@
-function [assigned_states] = executa_segmentacao();
 
-close all;
-clear all;
+
+function assigned_states = executa_segmentacao(arquivo);
+% close all;
+% clear all;
 
 load("ambiente.mat")
-audio = audioread("../Banco_A/a0200.wav");
+
+path = strcat("../Banco_A/", arquivo);
+audio = audioread(path);
 [assigned_states] = runSpringerSegmentationAlgorithm(audio, 2000, B_matrix, pi_vector, total_obs_distribution, true);
 
 end
--- Python/vaiQueDaCerto.py
+++ Python/vaiQueDaCerto.py
@@ -1,6 +1,23 @@
-import matlab.engine
-eng = matlab.engine.start_matlab()
-eng.cd(r'..\\Matlab_segmentacao')
-eng.joao_script(nargout=1)
+import matlab.engine 
+import psutil, os #utilizado para colocar o processo em alta prioridade
+import sys
 
-eng.quit()
+def elevate_process_priority ():
+    p = psutil.Process(os.getpid())
+    p.nice(psutil.HIGH_PRIORITY_CLASS)
+
+def call_matlab_script(file_name):
+    eng = matlab.engine.start_matlab()
+    eng.cd(r'..\\Matlab_segmentacao')
+    resultado = eng.joao_script(file_name,nargout=1)
+    variavel = resultado
+    eng.quit()
+
+    print(variavel)
+
+def main(argumento):
+    # print(argumento)
+    elevate_process_priority()
+    call_matlab_script(argumento)
+
+main(sys.argv[1])
