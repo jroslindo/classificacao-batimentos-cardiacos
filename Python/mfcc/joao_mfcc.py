f472b34e7aef98b7adfa3a55157c45d20e6f277b
João Paulo Roslindo <jroslindotab@gmail.com>
2020-07-27 16:05:59 -0300

    Começando a mfcc

--- Python/mfcc/joao_mfcc.py
+++ Python/mfcc/joao_mfcc.py
@@ -4,10 +4,10 @@ import scipy.io.wavfile as wav
 
 
 def main():
-    (rate,sig) = wav.read("..\\..\\Banco_A\\a0001.wav")
+    (rate,sig) = wav.read("..\\segmentacao\\resultado.wav")
     mfcc_feat = mfcc(sig,rate)
     fbank_feat = logfbank(sig,rate)
 
-    print(len(fbank_feat[0])) #[1:3,:]
+    print(fbank_feat) #[1:3,:]
 
 main()
