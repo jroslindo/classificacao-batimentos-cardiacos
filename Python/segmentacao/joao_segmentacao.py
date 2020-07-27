76a9f86e5b149d318bb3f62ceea147360a08da34
João Paulo Roslindo <jroslindotab@gmail.com>
2020-07-27 13:25:56 -0300

    Corrigindo a segmentação no arquivo python

--- Python/segmentacao/joao_segmentacao.py
+++ Python/segmentacao/joao_segmentacao.py
@@ -8,21 +8,20 @@ def elevate_process_priority ():
 
 def call_matlab_script(file_name):
     eng = matlab.engine.start_matlab()
-    eng.cd(r'..\\Matlab_segmentacao')
+    eng.cd(r'..\\..\\Matlab_segmentacao')
     resultado = eng.joao_script(file_name,nargout=1)
     eng.quit()
 
     arquivo = open("resultado.txt", "w")
     for i in resultado:
         arquivo.write(str(i[0])+"\n")
-
     arquivo.close()
 
 def main(argumento):
     elevate_process_priority()
     call_matlab_script(argumento)
-
     print("fim")
+    
     
 
 main(sys.argv[1])
