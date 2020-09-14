a290645a5c0b53a61fc9ce8e98e073d59889ff27
João Paulo Roslindo <jroslindotab@gmail.com>
2020-09-14 12:29:29 -0300

    começando graficos da rede
    quase concluido

--- Python/RNA/treino_RNA.py
+++ Python/RNA/treino_RNA.py
@@ -15,28 +15,23 @@ target = torch.load('target.pt')
 entrada, entrada_validacao , respostas_vetor, respostas_vetor_validacao = train_test_split(data, target, test_size=0.2, random_state=42)
 entrada.requires_grad_()
 
-graph_error = []
-graph_x     = []
-j=0
+grafico_treino = []
+grafico_validacao = []
+grafico_x     = []
 
 print("Começando o treino")
 running_loss = 0.0
 
-for epoch in range(100):  # loop over the dataset multiple times
+for epoch in range(50):  # loop over the dataset multiple times
     print("epoca " + str(epoch))
+    model.train()
+    running_loss = 0.0
     for i in range(len(entrada)):
-        entrada_aux = entrada[i].unsqueeze(0)
+        # entrada_aux = entrada[i].unsqueeze(0)
 
         optimizer.zero_grad()
 
-        outputs = model(entrada_aux.unsqueeze(0))
-
-        # if i == 0:
-        #     print(outputs)
-        #     print(respostas_vetor[i])
-        #     print("\n--------------------------\n")
-
-        # loss = criterion(outputs, respostas_vetor[i].unsqueeze(0))
+        outputs = model(entrada[i].unsqueeze(0).unsqueeze(0))
         loss = criterion(outputs.unsqueeze(0), respostas_vetor[i].unsqueeze(0)) #respostas_vetor[i].unsqueeze(0)
 
         if i == 0:
@@ -45,14 +40,30 @@ for epoch in range(100):  # loop over the dataset multiple times
             loss.backward(retain_graph=True)
 
         optimizer.step()
-
         running_loss += loss.item()
 
-    if (epoch % 10 == 9):
-        print(str(running_loss/(len(entrada)*epoch)) + "\n---------------------\n")
-        graph_error.append(running_loss/(len(entrada)*epoch))
-        graph_x.append(j)
-        j+=1
+    grafico_treino.append(running_loss/len(entrada))
+    #VALIDANDOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+    running_loss = 0.0
+    model.eval()
+    for i in range (len(entrada_validacao)):
+
+        outputs = model(entrada_validacao[i].unsqueeze(0).unsqueeze(0))
+        loss = criterion(outputs.unsqueeze(0), respostas_vetor_validacao[i].unsqueeze(0))
+        running_loss += loss.item()
+
+        # if predicted[1][0].item() == respostas_vetor_validacao[i].item():
+        #     certos += 1
+        # else:        
+        #     errados += 1  
+
+    grafico_validacao.append(running_loss/len(entrada_validacao))
+    grafico_x.append(epoch)
+    # if (epoch % 10 == 9):
+    #     print(str(running_loss/(len(entrada)*epoch)) + "\n---------------------\n")
+    #     graph_error.append(running_loss/(len(entrada)*epoch))
+    #     graph_x.append(j)
+    #     j+=1
 
 
 
@@ -60,9 +71,13 @@ print('Finished Training:  ' + str(running_loss/(len(entrada)*epoch)) + " --Salv
 
 torch.save(model.state_dict(), "net.pth")
 
-plt.plot(graph_x, graph_error)
+plt.subplot(2,1,1)
+plt.plot(grafico_x, grafico_treino)
+
+plt.subplot(2,1,2)
+plt.plot(grafico_x, grafico_validacao)
+
 plt.show()
-plt.waitforbuttonpress()
 
 
 
@@ -74,41 +89,42 @@ plt.waitforbuttonpress()
 
 
 
-print("validando: \n\n")
 
-model.load_state_dict(torch.load("net.pth"))
-model.eval()
-desconhecido = 0
-certos = 0
-errados = 0
-resposta = 0
+# print("validando: \n\n")
 
-arq = open("log.txt", "w")
+# model.load_state_dict(torch.load("net.pth"))
 
-for i in range (len(entrada_validacao)):
+# desconhecido = 0
+# certos = 0
+# errados = 0
+# resposta = 0
 
-    outputs = model(entrada_validacao[i].unsqueeze(0).unsqueeze(0))
-    predicted = torch.max(outputs.unsqueeze(0), 1)
-    arq.write(str(outputs) + "\n")
-    # print (torch.max(outputs.unsqueeze(0), 1))
-    arq.write("previsto: " + str(predicted[1][0].item()) + "\n")
-    arq.write("resposta: " + str(respostas_vetor_validacao[i].item())+ "\n")
-    # input("enter")
+# arq = open("log.txt", "w")
 
-    if predicted[1][0].item() == respostas_vetor_validacao[i].item():
-        arq.write("certo"+ "\n")
-        certos += 1
-    else:        
-        arq.write("errado"+ "\n")
-        errados += 1
+# for i in range (len(entrada_validacao)):
+
+#     outputs = model(entrada_validacao[i].unsqueeze(0).unsqueeze(0))
+#     predicted = torch.max(outputs.unsqueeze(0), 1)
+#     arq.write(str(outputs) + "\n")
+#     # print (torch.max(outputs.unsqueeze(0), 1))
+#     arq.write("previsto: " + str(predicted[1][0].item()) + "\n")
+#     arq.write("resposta: " + str(respostas_vetor_validacao[i].item())+ "\n")
+#     # input("enter")
+
+#     if predicted[1][0].item() == respostas_vetor_validacao[i].item():
+#         arq.write("certo"+ "\n")
+#         certos += 1
+#     else:        
+#         arq.write("errado"+ "\n")
+#         errados += 1
     
-    arq.write("--------------\n\n")
+#     arq.write("--------------\n\n")
 
 
 
 
 
-print("total: " + str(len(entrada_validacao))+ "\n")
-print("Certos: " + str(certos) + " Porcentagem: " + str(certos/len(entrada_validacao))+ "\n")
-print("Errados: " + str(errados) + " Porcentagem: " + str(errados/len(entrada_validacao))+ "\n")
-print("Desconhecido: " + str(desconhecido) + " Porcentagem: " + str(desconhecido/len(entrada_validacao))+ "\n")
+# print("total: " + str(len(entrada_validacao))+ "\n")
+# print("Certos: " + str(certos) + " Porcentagem: " + str(certos/len(entrada_validacao))+ "\n")
+# print("Errados: " + str(errados) + " Porcentagem: " + str(errados/len(entrada_validacao))+ "\n")
+# print("Desconhecido: " + str(desconhecido) + " Porcentagem: " + str(desconhecido/len(entrada_validacao))+ "\n")
